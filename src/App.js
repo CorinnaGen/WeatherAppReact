d96027a3c56229ae3afa50887bd570c8155c2cbd
CorinnaGen <77247943+CorinnaGen@users.noreply.github.com>
2021-04-18 10:07:43 +0200

    css

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
         <h1>
           {" "}
           Bari <span id="heading"></span>
-          <img id="main-icon" src="" alt="sun" />
+          <img id="main-icon" src="" alt="weather" />
           <span id="description"></span>
         </h1>
         <Date />
