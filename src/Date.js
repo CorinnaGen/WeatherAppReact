87403742f608c9d90066d9bd49811331ab53634d
CorinnaGen <77247943+CorinnaGen@users.noreply.github.com>
2021-04-25 10:24:43 +0200

    .

--- src/Date.js
+++ src/Date.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./Date.css";
 
 export default function Date(props) {
-    let days=["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]
-    let date= days[props.date.getDate()];
+    let days=["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
+    let day= days[props.date.getDay()];
     let hours=props.date.getHours();
     if (hours <10){
       hours= `0${hours}`;
@@ -14,7 +14,7 @@ export default function Date(props) {
     }
    
     return (<div className="Date">
-      <h2 id="current-time">¨{date} {hours}:{minutes}</h2>
+      <h2 id="current-time">{day} {hours}:{minutes}</h2>
       <br />
     </div>
   );
