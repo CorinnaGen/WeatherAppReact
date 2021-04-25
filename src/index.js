23cc9610b6f8a8f0145a919e9b1073e47d18a8d8
CorinnaGen <77247943+CorinnaGen@users.noreply.github.com>
2021-04-25 09:45:21 +0200

    .

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App defaultCity="Bari"/>
+    <App defaultCity="Rome"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
