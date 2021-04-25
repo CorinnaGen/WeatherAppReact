dace6e4c2fba09b5d36a462670fb48f2b5dcdf2e
CorinnaGen <77247943+CorinnaGen@users.noreply.github.com>
2021-04-25 18:27:16 +0200

    date2

--- src/Date.js
+++ src/Date.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Date.css";
 
-export default function Date(props) {
+export default function formattedDate(props) {
     let days=["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
     let day= days[props.date.getDay()];
     let hours=props.date.getHours();
